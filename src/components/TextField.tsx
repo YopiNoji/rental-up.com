ea6014c64dc771635aecb4daa0ffcf1ed7556a68
yopinoji <yopinoji@yahoo.co.jp>
2020-09-26 23:26:54 +0900

    improve accessibility of site

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -12,8 +12,8 @@ const TextField: React.FC<PropTypes> = ({label, require}) => {
             <label className="block">
                 <span className="text-deep-gray text-sm mr-2">{label}</span>
                 {require && <span className="rounded bg-main-orange text-xs px-1">必須</span>}
+                <input type="text" className="block border border-light-gray shadow-inner rounded" />
             </label>
-            <input type="text" className="block border border-light-gray shadow-inner rounded" />
         </div>
     </>
   );
