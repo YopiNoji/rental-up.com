5688accdaa341fbf0731d7fee1ba93b03da8e34d
yopinoji <yopinoji@yahoo.co.jp>
2020-09-29 14:49:03 +0900

    improve SEO

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -7,6 +7,7 @@ type PropTypes = {
   description: string;
   image?: string;
   type?: "website" | "article";
+  twitterUser?: "";
 };
 
 const Header: React.FC<PropTypes> = ({
@@ -15,16 +16,23 @@ const Header: React.FC<PropTypes> = ({
   description,
   image,
   type,
+  twitterUser,
 }) => {
   return (
     <Helmet>
       <html lang={lang || "ja"} />
       <title lang={lang || "ja"}>{title}</title>
       <meta name="description" content={description} />
+      <meta property="og:site_name" content="サイト名" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:type" content={type || "website"} />
-      <meta property="og:image" content={image || "assets/ogp.png"} />
+      <meta property="og:image" content={image || "/assets/ogp.png"} />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content={twitterUser} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image || "/assets/ogp.png"} />
     </Helmet>
   );
 };
